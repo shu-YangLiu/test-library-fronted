8b4b15c88068eb723751945ee016dae995f99c84
shu-Yangliu <ly8219181@126.com>
2019-05-23 23:21:41 +0800

    Create Question.vue

---
+++ src/view/Question.vue
@@ -0,0 +1,62 @@
+<template>
+  <Row style="background:#eee;padding:20px">
+    <Col span="11">
+      <Card v-for="(paper,index) in paperlist" :key="paper" :label="paper" style="margin:15px">
+        <p slot="title">{{index+1}} {{paper.text}}</p>
+        <!-- <Col span="8"
+            <p slot="title">科目：{{paper.subject}}</p>
+        </Col>
+        <Col span="8">
+            <p slot="title">年级：{{paper.grade}}</p>
+        </Col> -->
+        <p slot="title"  style="float:left;">科目：{{paper.subject}} </p> 
+        <p slot="title" style="width:80%;float:right;">年级：{{paper.grade}} </p>
+        <p>答案：{{paper.answer}}</p>
+        <p>id:{{paper.id}}</p>
+      </Card>
+      <div>
+        <Page :total="100" show-elevator/>
+      </div>
+    </Col>
+    <Col span="6" class="padding-left-10"></Col>
+  </Row>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      userInfo: JSON.parse(localStorage.getItem("userInfo")),
+      paperlist: ""
+    };
+  },
+  created: function() {
+    console.log(this.userInfo);
+    this.axios
+      .post("http://localhost:8000/test_library/question/")
+      .then(res => {
+        console.log(res.data);
+        this.paperlist = res.data;
+      })
+      .catch(res => {
+        console.log(res);
+      });
+  }
+};
+</script>
+
+<style>
+.ivu-row {
+  position: relative;
+  margin-left: 200px;
+  margin-right: 200px;
+  margin-bottom: 30px;
+  margin-top: 20px;
+  height: auto;
+  zoom: 1;
+  display: block;
+}
+.ivu-col-span-11 {
+  display: block;
+  width: 100%;
+}
+</style>
